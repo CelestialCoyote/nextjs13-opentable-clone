dfc0c24667011f5a6cdf36220795766d1d58a92c
Paul Stearns <celestialcoyote@gmail.com>
2023-06-06 17:26:45 -0700

    created nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			
+		},
+		fontSize: {
+			"2xsm": "10px",
+			xsm: "12px",
+			sm: "13px",
+			reg: "14px",
+			lg: "18px",
+			"2xl": "22px",
+			"3xl": "25px",
+			"4xl": "32px",
+			"5xl": "40px",
+			"6xl": "50px",
+			"7xl": "70px",
+		}
+	},
+	plugins: [],
 }
