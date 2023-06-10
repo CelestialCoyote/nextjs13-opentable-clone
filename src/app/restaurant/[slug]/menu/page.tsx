fba748ca686080847d1077549d0e4ad52d743da8
Paul Stearns <celestialcoyote@gmail.com>
2023-06-10 15:27:34 -0700

    favicon change does not work

--- src/app/restaurant/[slug]/menu/page.tsx
+++ src/app/restaurant/[slug]/menu/page.tsx
@@ -1,22 +1,14 @@
 import Menu from "@/app/components/Menu";
-import NavBar from "@/app/components/NavBar";
-import Header from "@/app/components/Header";
 import RestaurantNavBar from "@/app/components/RestaurantNavBar";
 
 
 export default function RestaurantMenu() {
 	return (
-		<main className="bg-gray-100 min-h-screen w-screen">
-			<main className="max-w-screen-2xl m-auto bg-white">
-				<NavBar />
-				<Header />
-				<div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-					<div className="bg-white w-[100%] rounded p-3 shadow">
-						<RestaurantNavBar />
-						<Menu />
-					</div>
-				</div>
-			</main>
-		</main>
+		<>
+			<div className="bg-white w-[100%] rounded p-3 shadow">
+				<RestaurantNavBar />
+				<Menu />
+			</div>
+		</>
 	)
 }
--- src/app/restaurant/[slug]/page.tsx
+++ src/app/restaurant/[slug]/page.tsx
@@ -1,6 +1,4 @@
-import NavBar from "@/app/components/NavBar";
 import RestaurantDescription from "@/app/components/RestaurantDescription";
-import RestaurantHeader from "@/app/components/RestaurantHeader";
 import RestaurantImages from "@/app/components/RestaurantImages";
 import RestaurantNavBar from "@/app/components/RestaurantNavBar";
 import RestaurantRating from "@/app/components/RestaurantRating";
@@ -11,24 +9,18 @@ import RestaurantTitle from "@/app/components/RestaurantTitle";
 
 export default function RestaurantDetails() {
 	return (
-		<main className="bg-gray-100 min-h-screen w-screen">
-			<main className="max-w-screen-2xl m-auto bg-white">
-				<NavBar />
-				<RestaurantHeader />
-				<div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-					<div className="bg-white w-[70%] rounded p-3 shadow">
-						<RestaurantNavBar />
-						<RestaurantTitle />
-						<RestaurantRating />
-						<RestaurantDescription />
-						<RestaurantImages />
-						<RestaurantReviews />
-					</div >
-					<div className="w-[27%] relative text-reg">
-						<RestaurantReservationCard />
-					</div>
-				</div>
-			</main>
-		</main>
+		<>
+			<div className="bg-white w-[70%] rounded p-3 shadow">
+				<RestaurantNavBar />
+				<RestaurantTitle />
+				<RestaurantRating />
+				<RestaurantDescription />
+				<RestaurantImages />
+				<RestaurantReviews />
+			</div >
+			<div className="w-[27%] relative text-reg">
+				<RestaurantReservationCard />
+			</div>
+		</>
 	)
 }
